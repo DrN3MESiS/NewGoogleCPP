6e6ae70272bbe8845203c7f0b976cc5c15566962
Alan Enrique Maldonado Navarro <mlgalanmn88@gmail.com>
2018-11-14 19:28:21 -0600

    Update NewGoogleSearchEngine.cpp

--- NewGoogleSearchEngine.cpp
+++ NewGoogleSearchEngine.cpp
@@ -4,6 +4,94 @@
 
 using namespace std;
 
-int main(){
+class Node{
+	public:
+		
+	char val;
+	Node *X[25];
+	bool isWord;
 	
+	Node(char c = '#', bool isWord = false){
+		this->val = c;
+		this->isWord = isWord;
+		
+		for(int i = 0; i < 25; i++){
+			this->X[i] = NULL;
+		}
+	}
+};
+
+class Trie{
+	private:
+		Node *H[25];
+		
+	public:
+		
+	Trie(){
+		for(int i = 0; i < 25; i++){
+			this->H[i] = NULL;
+		}
+	}
+	
+	void insertWord(string word){
+		int k = word[0] - 'a';
+		
+		if(H[k] == NULL){
+			H[k] = new Node(word[0], false);
+		}
+		
+		Node *curNode = H[k];
+		
+		for(int i = 1; i < word.length(); i++){
+			k = word[i] - 'a';
+			if(curNode->X[k] == NULL){
+				curNode->X[k] = new Node(word[i], false);
+			} else {
+				curNode = curNode->X[k];
+			}
+		}
+		
+		curNode->isWord = true;
+	}
+	
+	bool findWord(string word){
+		int k = word[0] - 'a';
+		
+		if(H[k] == NULL){
+			return false;
+		}
+		
+		Node *curNode = H[k];
+		
+		for(int i = 1; i < word.length(); i++){
+			k = word[i] - 'a';
+			if(curNode->X[k] == NULL){
+				return false;
+			} else {
+				curNode = curNode->X[k];
+			}
+		}		
+		return curNode->isWord;
+	}
+};
+
+int main(){
+	Trie trie;
+	
+	trie.insertWord("hola");
+	cout << trie.findWord("hola") << endl;
+	
+	int wordCount;
+	cin >> wordCount;
+	
+	for(int i = 0; i < wordCount; i++){
+		string wordToInsert;
+		cin >> wordToInsert;
+		
+		trie.insertWord(wordToInsert);
+	}
+	
+	string wordToSearch;
+	
+	return false;
 }
