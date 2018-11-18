9034cfa993e82d8109c80bc0208eda22e3ffd3df
Alan Enrique Maldonado Navarro <mlgalanmn88@gmail.com>
2018-11-18 00:31:10 -0600

    Comentarios

--- NewGoogleSearchEngine.cpp
+++ NewGoogleSearchEngine.cpp
@@ -5,7 +5,7 @@
 
 using namespace std; 
   
-const int ALPHABET_SIZE = 26; 
+const int ALPHABET_SIZE = 25; 
   
 // trie node 
 struct TrieNode 
@@ -33,35 +33,54 @@ struct TrieNode *getNode(void)
 // If not present, inserts key into trie 
 // If the key is prefix of trie node, just 
 // marks leaf node 
-void insert(struct TrieNode *root, string key) 
+void insert(struct TrieNode *root, string key) //La funcion requiere la variable del Trie y la palabra a analizar
 { 
-    struct TrieNode *pCrawl = root; 
+    struct TrieNode *pCrawl = root;  //Cada letra se convierte en su propio alphabet
   
-    for (int i = 0; i < key.length(); i++) 
+    for (int i = 0; i < key.length(); i++) //Toma la palabra, y la examina, letra por letra.
     { 
-        int index = key[i] - 'a'; 
-        if (!pCrawl->children[index]) 
-            pCrawl->children[index] = getNode(); 
+        int index = key[i] - 'a'; //Calcula la posicion de la primera letra del string en el arreglo del alphabet
+        if (!pCrawl->children[index])  //Si la letra esta dentro del abecedario, no hace nada, pero si no esta, llama a crear 
+            pCrawl->children[index] = getNode(); //agregamos el hijo que no tiene
   
-        pCrawl = pCrawl->children[index]; 
+        pCrawl = pCrawl->children[index]; //el nuevo inicio sera la letra en la que se quedo
     } 
   
-    // mark last node as leaf 
-    pCrawl->isEndOfWord = true; 
+    pCrawl->isEndOfWord = true; //El ultimo nodo se convierte en un nuevo hijo.
 } 
   
 // Returns true if key presents in trie, else 
 // false 
 bool search(struct TrieNode *root, string key) 
 { 
-    struct TrieNode *pCrawl = root; 
+    struct TrieNode *pCrawl = root; //Cada letra se convierte en su propio alphabet
   
     for (int i = 0; i < key.length(); i++) 
     { 
         int index = key[i] - 'a'; 
-        if (!pCrawl->children[index]) 
+        if (!pCrawl->children[index])
+        {
             return false; 
+        }
+        
+        pCrawl = pCrawl->children[index]; 
+    } 
   
+    return (pCrawl != NULL && pCrawl->isEndOfWord); 
+} 
+
+bool searchAdyacent(struct TrieNode *root, string key) 
+{ 
+    struct TrieNode *pCrawl = root; //Cada letra se convierte en su propio alphabet
+  
+    for (int i = 0; i < key.length(); i++) 
+    { 
+        int index = key[i] - 'a'; 
+        if (!pCrawl->children[index])
+        {
+            return false; 
+        }
+        
         pCrawl = pCrawl->children[index]; 
     } 
   
@@ -74,12 +93,12 @@ int main()
     int I_COUNT, Q_COUNT;
 	cin >> I_COUNT >> Q_COUNT;
     
-    string keys[I_COUNT]; //arreglo que tendra todas las palabras del usuario 
+    string wordsToInsert[I_COUNT]; //arreglo que tendra todas las palabras del usuario 
     string prefix[Q_COUNT]; //arreglo que guardara todos los prefijos que se preguntaran
     
     for(int i = 0; i < I_COUNT; i++)
     {
-		cin >> keys[i]; //ingresar cada una de las palabras al diccionario
+		cin >> wordsToInsert[i]; //ingresar cada una de las palabras al diccionario
 	}
 	
 	for(int i = 0; i < Q_COUNT; i++)
@@ -92,7 +111,7 @@ int main()
     // Funcion para construir el Diccionario
     for (int i = 0; i < I_COUNT; i++)
     {
-        insert(root, keys[i]); 
+        insert(root, wordsToInsert[i]); 
     }
     
     // Buscar prefijo dentro del Trie
@@ -100,12 +119,14 @@ int main()
     {
         if(search(root, prefix[i]))
         {
-            cout << "True";
+            cout << "La palabra si esta en el Trie (Esto no es un prefijo) == " << prefix[i] << "\n";
         }
             else 
         {
-            cout << "False";
+            cout << "La palabra no esta en el Trie (Esto no es un prefijo) == " << prefix[i] << "\n";
         }
+        
+        searchAdyacent(root, prefix[i];
     }
     return 0; 
 } 
